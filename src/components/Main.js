ea9416ceb347ff9571306a6985628780edf26d92
Alexandr Raspertov <raspertovalexandr@yandex.ru>
2021-04-09 18:43:17 +0300

    fix video

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,6 @@ import { useState } from "react";
 import SearchForm from "./SearchForm";
 import SearchResult from "./SearchResult";
 import { searchApi, resultApi } from "../api";
-import video from "../video/videoplayback.webm"
 
 export default function Main() {
   const [search, setSearch] = useState("");
@@ -59,7 +58,7 @@ export default function Main() {
     <>
       <div className="background">
         <video autoPlay muted loop className="background__video">
-          <source src={video} type="video/webm"/>
+          <source src="https://r2---sn-4g5ednsd.googlevideo.com/videoplayback?expire=1618004083&amp;ei=E3RwYMqDBeumx_APk5qemAc&amp;ip=2.58.12.187&amp;id=o-AF5aAcBZR7rUadOsFpv1IYYFuc_mSmjBXwt6hyU-p_wx&amp;itag=248&amp;aitags=133%2C134%2C135%2C136%2C137%2C160%2C242%2C243%2C244%2C247%2C248%2C278&amp;source=youtube&amp;requiressl=yes&amp;vprv=1&amp;mime=video%2Fwebm&amp;ns=qZeT2RqwpsAbObW75Y6TAEYF&amp;gir=yes&amp;clen=72977407&amp;dur=362.987&amp;lmt=1543619952498029&amp;fvip=3&amp;keepalive=yes&amp;fexp=24001373,24007246&amp;c=WEB&amp;txp=5432432&amp;n=X8yvkMhSvM3GDUdR&amp;sparams=expire%2Cei%2Cip%2Cid%2Caitags%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cns%2Cgir%2Cclen%2Cdur%2Clmt&amp;sig=AOq0QJ8wRQIhAP_Y6rV6TitW0wFAnPH8Q8LBMiEsrRmWQYo7C2dXu0gQAiB2daR_TrwnvzRd314kK9gavOXxXxk6zniWpaTM5o68Xw%3D%3D&amp;redirect_counter=1&amp;cm2rm=sn-5hnelr7s&amp;req_id=ac49c2a14fb4a3ee&amp;cms_redirect=yes&amp;mh=xc&amp;mip=46.188.32.100&amp;mm=34&amp;mn=sn-4g5ednsd&amp;ms=ltu&amp;mt=1617981771&amp;mv=D&amp;mvi=3&amp;pl=0&amp;lsparams=mh,mip,mm,mn,ms,mv,mvi,pl&amp;lsig=AG3C_xAwRAIgelX3-3qP1QBoe_X804Ckcqe91vyt9ACCRb9ISwZz-l0CIA1ZL2L8LYiZtmrdXY0xNXGiBoudkqqWmC3vDZrpFhtI&amp;ir=1&amp;rr=12" type="video/webm"/>
         </video>
       </div>
       <main className="main">
